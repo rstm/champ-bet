cbd203bc94c1da9f5a9dacacd717594b3eba274d
Rustem Mazitov <rmazitov@d-l-s.ru>
2018-06-14 00:38:01 +0300

    Fix exit

--- exit.php
+++ exit.php
@@ -4,12 +4,6 @@
 setcookie("user_id", "", time()-3600);
 setcookie("username", "", time()-3600);
 
-// unset($_COOKIE['user_id']);
-// unset($_COOKIE['username']);
-// // $config['cookie_domain']    = "signup.ru";
-// // $config['cookie_path']      = "signup.ru";
-// setcookie('user_id', '', -1, '/');
-// setcookie('username', '', -1, '/');
-$home_url = 'http://' . $_SERVER['HTTP_HOST']  . '/signup.ru/index.php';
- header('Location: ' . $home_url);
+// $home_url = 'http://' . $_SERVER['HTTP_HOST']  . '/signup.ru/index.php';
+ header('Location: index.php');
 ?>
