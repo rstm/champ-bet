748c49dde74f499ecc38e105610db6b22abf94d3
Rustem Mazitov <rmazitov@d-l-s.ru>
2018-06-14 22:01:18 +0300

    add admin

---
+++ admin.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+</head>
+<body>
+
+
+<?php
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+if ($server != null)
+	$dbc = mysqli_connect($server, $username, $password, $db);
+else
+	$dbc = mysqli_connect('localhost', 'root', '', 'lesson');
+
+
+    $query ="SELECT * FROM `matches`";
+     
+    $result = mysqli_query($dbc, $query) or die("Ошибка " . mysqli_error($dbc)); 
+    if($result)
+    {
+        $rows = mysqli_num_rows($result); // количество полученных строк
+         
+        echo "<table class='redTable'>";
+        while($match = mysqli_fetch_object($result)
+        {
+            echo "<tr>";
+
+            $command1name = ucfirst(strtolower($match->command1));
+            $command2name = ucfirst(strtolower($match->command2));
+            
+            echo "<td id='match$match->id' class='match_number'>#$match->id<br>$match->datetime</td>";
+            echo "<td><div><img src=\"img/icons/$command1name.png\"></br><div class='match_2'>$match->command1</div></div><br><div class='score'>$match->score1</div>
+            </div></td>";
+            echo "<td><div class='match_3'>VS.</div></td>";
+            echo "<td><div><img src=\"img/icons/$command2name.png\"></br><div class='match_2'>$match->command2</div></div><br><div class='score'>$match->score2</div></td>";
+
+            if (strtotime('+3 hours', time()) < strtotime($match->datetime)) {
+            ?>
+            
+            <td>
+                  <form method="POST" action="set_result.php">
+                    <input type="hidden" name="match_id" value="<?=$match->id?>">
+                    <input class="rate-input inputcss" type="number" name="score1" >-
+                    <input class="rate-input inputcss" type="number" name="score2" ><br>
+                    <button id="baton" type="submit" name="submit">Сохранить результат</button>
+                  </form>
+              </td>
+
+            <?php
+            } else echo "<td class='match_number'>Ставок больше нет</td>";
+             
+             echo "</tr>";
+        }
+       
+        echo "</table>";
+         
+        // очищаем результат
+        mysqli_free_result($result);
+    }
+     
+    mysqli_close($dbc);
+?>
+
+</body>
+<html>
---
+++ set_result.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ERROR</title>
+<link href="style/style1.css" rel="stylesheet">
+</head>
+
+ <body>
+
+<?php
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+if ($server != null)
+	$dbc = mysqli_connect($server, $username, $password, $db) OR DIE('Ошибка подключения к базе данных');
+else
+	$dbc = mysqli_connect('localhost', 'root', '', 'lesson') OR DIE('Ошибка подключения к базе данных');
+
+if(isset($_POST['match_id'])){
+	$user_id = $_COOKIE['user_id'];
+	$match_id = mysqli_real_escape_string($dbc, trim($_POST['match_id']));
+	$score1 = mysqli_real_escape_string($dbc, trim($_POST['score1']));
+	$score2 = mysqli_real_escape_string($dbc, trim($_POST['score2']));
+
+	if(!empty($match_id) && $score1 != null && $score2 != null && $score1 > -1 && $score2 > -1) {
+        $query ="UPDATE `matches` SET score1 = $score1, score2 = $score2 where match_id = $match_id";
+		mysqli_query($dbc,$query);
+		mysqli_close($dbc);
+	 	header("Location: admin.php");
+	}
+	else {
+		echo "<h2>Ошибка, попробуйте ещё раз! </h2>";
+		echo "<div class='a2'>1)Возможно вы не ввели одно из значений</div>";
+		echo "<div class='a2'>2)Возможно вы ввели недопустимое значение</div>";
+		echo "<div class='a2'>Если не получилось исправить, обратитесь к Демиду!</div>";
+
+	}
+}
+?>
+
+
+ 
+</body>
+  
+  
+</body>
+</html>
